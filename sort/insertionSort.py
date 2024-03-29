6cf36e79651593af5f15fd75f7c90617e22de4d0
SeHun <davidpark8575@gmail.com>
2024-03-29 18:22:21 +0900

    renaming & add some algorithm

---
+++ sort/insertionSort.py
@@ -0,0 +1,12 @@
+def insertionSort(nums):
+    for i in range(1, len(nums)):
+        key = nums[i]
+        j = i -1
+        while j >= 0 and key < nums[j]:
+            nums[j+1] = nums[j]
+            j-=1
+        nums[j+1] = key
+
+nums = [5,4,9,7,0,3,4,2,5,7,9,10,2,6,5]
+insertionSort(nums)
+print(nums)
