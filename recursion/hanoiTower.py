8a21d30f92290b89a67da923dac7b9e5045e1d8c
SeHun <davidpark8575@gmail.com>
2024-03-30 11:25:21 +0900

    hanoiTower.py 수정

--- recursion/hanoiTower.py
+++ recursion/hanoiTower.py
@@ -1,8 +1,21 @@
 # 하노이 탑 hanoi tower
-def hanoiTower(a,b,c,n,m,cnt):
+
+# 기본
+def hanoiTower(a,b,c,n,):
+    if (n == 1):
+        print(f"{a} {c}")
+    else:
+        hanoiTower(a,c,b,n-1)
+        print(f"{a} {c}")
+        hanoiTower(b,a,c,n-1)
+
+# 특정 순서 정할 때
+def hanoiTower2(a,b,c,n,m,cnt):
     if (m == cnt):
         print(f"{a} {c}")
         return None
     else:
-        hanoiTower(a,c,b,n-1,m,cnt+1)
-        hanoiTower(b,a,c,n-1,m,cnt+1)
+        hanoiTower2(a,c,b,n-1,m,cnt+1)
+        hanoiTower2(b,a,c,n-1,m,cnt+1)
+
+hanoiTower("A", "B", "C", 3)
