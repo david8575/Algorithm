c2b464771ae244fb977336adf91712170dbd6bb4
SeHun <davidpark8575@gmail.com>
2024-04-06 12:09:53 +0900

    알고리즘 추가

--- sort/insertionSort.py
+++ sort/insertionSort.py
@@ -1,7 +1,7 @@
 def insertionSort(nums):
     for i in range(1, len(nums)):
         key = nums[i]
-        j = i -1
+        j = i - 1
         while j >= 0 and key < nums[j]:
             nums[j+1] = nums[j]
             j-=1
