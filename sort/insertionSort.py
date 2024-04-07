cac0ef88e56afdf97c26856554cf48e305d41a0d
SeHun <davidpark8575@gmail.com>
2024-04-07 14:57:59 +0900

    insertsort 수정

--- sort/insertionSort.py
+++ sort/insertionSort.py
@@ -1,3 +1,4 @@
+import time
 def insertionSort(nums):
     for i in range(1, len(nums)):
         key = nums[i]
@@ -6,7 +7,8 @@ def insertionSort(nums):
             nums[j+1] = nums[j]
             j-=1
         nums[j+1] = key
+        print(f'{i}번 정렬{nums}')
+        time.sleep(1)
 
 nums = [5,4,9,7,0,3,4,2,5,7,9,10,2,6,5]
 insertionSort(nums)
-print(nums)
